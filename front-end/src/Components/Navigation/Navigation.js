f2168b4f16de965e8ee52b75185e4daeab536948
Andre Timm <andretimm2012@gmail.com>
2019-02-11 09:30:25 -0200

    preparando componente para receber dados da api

--- front-end/src/Components/Navigation/Navigation.js
+++ front-end/src/Components/Navigation/Navigation.js
@@ -4,16 +4,52 @@ import './Styles.css';
 import Header from '../Header/Header';
 
 export default class Navigation extends Component {
+
+    state = {
+        active: '#0000',
+        orders: [
+            {
+                number: '#0000',
+                customer: 'Luke ​Skywalker'
+            },
+            {
+                number: '#0001',
+                customer: 'Darth ​Vader',
+            },
+            {
+                number: '#0002',
+                customer: 'Han ​Solo',
+            },
+            {
+                number: '#0003',
+                customer: 'Imperador ​Palpatine',
+            },
+        ]
+    };
+
+    handleChanceActive = (e) => {
+        e.preventDefault();
+        const order =  e.currentTarget.id;
+        this.setState({active: order});
+    }
+
     render() {
         return (
             <nav>
                 <ul>
                     <li><Header /></li>
-                    <li><a className="active" href="#home">#0000 - Luke ​Skywalker</a></li>
-                    <li><a href="#news">#0001 - Darth ​Vader</a></li>
-                    <li><a href="#contact">#0002 - Han ​Solo</a></li>
-                    <li><a href="#about">#0003 - Imperador ​Palpatine</a></li>
-                    <li><a href="#about">#0004 - Han ​Solo</a></li>
+                    {this.state.orders.map(order => (
+                        <li>
+                            <a 
+                                href="#customer"   
+                                id={order.number}                       
+                                onClick={this.handleChanceActive}
+                                className={order.number === this.state.active ? 'active' : ''}
+                                key={order.number}>
+                                {order.number} - {order.customer}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         );
